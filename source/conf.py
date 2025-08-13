6a4398e478f6faab4862c4a56986ee5b8d916895
Hang Lu <luhang@ict.ac.cn>
2025-08-13 17:12:51 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -33,3 +33,28 @@ html_css_files = [
 ]
 
 html_logo = '_static/logoS.png'
+
+# -------------------------- latex -------------------------------------------------
+latex_engine = 'xelatex'
+latex_elements = {
+    'passoptionstopackages': r'''
+\PassOptionsToPackage{svgnames}{xcolor}
+''',
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'sphinxsetup': 'TitleColor=DarkGoldenrod',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+latex_show_urls = 'footnote'
+# --------------------------------------------------------------------------------
